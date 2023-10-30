d0b2b987cee64d80f542490f3c93f1c43411b87f
Abhiram2212 <thurakaananthkumar46@gmail.com>
2023-10-30 21:09:13 +0530

    Preapre for Production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser";
 import { notFound,errorHandler } from "./middlewaare/errorMiddleware.js";
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import path from "path";
 
 connectDB();
 const app = express();
@@ -13,6 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser()); 
 app.use('/api/users',userRoutes);
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+  
 app.get('/',(req,res)=>res.send('Server is Ready'));
 app.use(notFound);
 app.use(errorHandler);
